298642ada463ffe8a74ce7609d84584435dbe2c3
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-08-27 09:35:03 -0300

    up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
 import ListagemProdutos from '@/components/ListagemProdutos.vue';
-import RodapeMobile from '@/components/RodapeMobile.vue';
 </script>
 
 <template>
   <ListagemProdutos />
-  <RodapeMobile />
 </template>
 
 <style scoped></style>
